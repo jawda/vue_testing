cc7c4da10328737f3d1e19db352ba0beefd0c014
joew <joew@quavermusic.com>
2021-07-08 16:18:53 -0500

    feat (basic_interactive): added slides and cur page property to use for visibility

---
+++ basic-interactive/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app" >
+<!--    <img src="./assets/slide-0.png" class="backgroundImg center ">-->
+<!--    <img src="viaJs" class="backgroundImg center ">-->
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <ul v-for="item in viaJSx" :key="item.id" v-show="curPage === item.id">
+      <Slide :id="item.id" :path="item.path"></Slide>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Slide from "@/components/Slide";
+export default {
+  name: 'App',
+  components: {
+    Slide
+  },
+  data() {
+    return {
+      viaJSx: [
+        {
+          id:0,
+          path: require('./assets/slide-0.png')
+        },
+        {
+          id:1,
+          path: require('./assets/slide-1.png')
+        }
+      ],
+      curPage: 0
+      // viaJs: [require('./assets/slide-0.png'),require('./assets/slide-1.png')] // eslint-disable-line no-undef
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ basic-interactive/src/components/Slide.vue
@@ -0,0 +1,32 @@
+<template>
+<div >
+  <img :src="path" class="backgroundImg center ">
+</div>
+</template>
+
+<script>
+export default {
+name: "Slide",
+props: {
+  id: Number,
+  path: String
+
+},
+
+
+}
+</script>
+
+<style scoped>
+
+.backgroundImg {
+  width: 100%;
+  height: auto;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+</style>
